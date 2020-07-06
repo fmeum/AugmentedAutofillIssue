6bff5a04eb3a781c2178b3a6f27c97988b7a6fb6
Fabian Henneke <fabian@henneke.me>
2020-07-06 19:04:50 +0200

    Initial commit

---
+++ app/src/main/java/com/chrome/beta/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.chrome.beta
+
+import android.app.Activity
+import android.content.ComponentName
+import android.content.Intent
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.View
+import android.widget.*
+import androidx.core.content.edit
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
+import androidx.core.text.italic
+import kotlin.system.exitProcess
+
+class MainActivity : Activity() {
+
+    companion object {
+        private const val PREFERENCE_IMPORTANT_FOR_AUTOFILL = "importantForAutofill"
+
+        private val EXPLANATION_BROKEN = buildSpannedString {
+            append("Autofill requests are correctly dispatched to the user-configured Autofill service for as long as the app is not restarted or a manual Autofill request is executed ")
+            bold {
+                append("if the first field tapped by the user is ")
+                italic {
+                    append("not")
+                }
+                append(" the URL bar.")
+            }
+            append("\n\nAutofill requests never reach the user-configured Autofill service (and go to the augmented Autofill service instead) for as long as the app is not restarted or a manual Autofill request is executed ")
+            bold {
+                append("if the first field tapped by the user is the URL bar.")
+            }
+        }
+
+        private val EXPLANATION_WORKAROUND = buildSpannedString {
+            append("Autofill requests are always correctly dispatched to the user-configured Autofill service.")
+        }
+    }
+
+    private val urlBar: EditText by lazy { findViewById<EditText>(R.id.url_bar) }
+    private val usernameField: EditText by lazy { findViewById<EditText>(R.id.username) }
+    private val passwordField: EditText by lazy { findViewById<EditText>(R.id.password) }
+    private val submit: Button by lazy { findViewById<Button>(R.id.submit) }
+    private val switchImportantForAutofill: Switch by lazy { findViewById<Switch>(R.id.switchImportantForAutofill) }
+    private val explanationText: TextView by lazy { findViewById<TextView>(R.id.explanationText) }
+
+    private var urlBarImportantForAutofill: Boolean
+        get() = PreferenceManager.getDefaultSharedPreferences(this).getBoolean(
+            PREFERENCE_IMPORTANT_FOR_AUTOFILL, false
+        )
+        set(value) {
+            PreferenceManager.getDefaultSharedPreferences(this).edit(commit = true) {
+                putBoolean(PREFERENCE_IMPORTANT_FOR_AUTOFILL, value)
+            }
+        }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_layout)
+
+        switchImportantForAutofill.isChecked = urlBarImportantForAutofill
+        urlBar.importantForAutofill =
+            if (urlBarImportantForAutofill) View.IMPORTANT_FOR_AUTOFILL_AUTO else View.IMPORTANT_FOR_AUTOFILL_NO
+        switchImportantForAutofill.setOnCheckedChangeListener { _, b ->
+            urlBarImportantForAutofill = b
+            restartApp()
+        }
+        explanationText.text =
+            if (urlBarImportantForAutofill) EXPLANATION_WORKAROUND else EXPLANATION_BROKEN
+
+        submit.setOnClickListener {
+            val url = urlBar.text.toString()
+            val username = usernameField.text.toString()
+            val password = passwordField.text.toString()
+            Toast.makeText(
+                applicationContext,
+                "Logged in to $url as $username with password $password",
+                Toast.LENGTH_LONG
+            ).show()
+            finish()
+        }
+    }
+
+    private fun restartApp() {
+        startActivity(Intent.makeRestartActivityTask(ComponentName(this, MainActivity::class.java)))
+        exitProcess(0)
+    }
+}
---
+++ app/src/main/java/com/chrome/beta/UrlBar.kt
@@ -0,0 +1,27 @@
+package com.chrome.beta
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.text.Editable
+import android.text.SpannableStringBuilder
+import android.util.AttributeSet
+import android.view.ViewStructure
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatEditText
+
+
+@SuppressLint("AppCompatCustomView")
+class UrlBar(context: Context, attrs: AttributeSet?) : EditText(context, attrs) {
+
+    private var providingAutofillStructure: Boolean = false
+
+    override fun onProvideAutofillStructure(structure: ViewStructure?, flags: Int) {
+        providingAutofillStructure = true
+        super.onProvideAutofillStructure(structure, flags)
+        providingAutofillStructure = false
+    }
+
+    override fun getText(): Editable? {
+        return if (providingAutofillStructure) SpannableStringBuilder("https://google.com") else super.getText()
+    }
+}
